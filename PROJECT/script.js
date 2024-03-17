c1f4c7234debe40d2a7316206463dfbfdbd10e06
369ineee <163739729+369ineee@users.noreply.github.com>
2024-03-17 21:51:45 +0800

    Add files via upload

---
+++ PROJECT/script.js
@@ -0,0 +1,16 @@
+let prevScrollpos = window.pageYOffset;
+const header = document.querySelector(".head1");
+
+window.addEventListener("scroll", function() {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    // scrolling up
+    header.style.top = "0";
+    header.classList.add("show");
+  } else {
+    // scrolling down
+    header.style.top = "-100px"; // Hide the header
+    header.classList.remove("show");
+  }
+  prevScrollpos = currentScrollPos;
+});
